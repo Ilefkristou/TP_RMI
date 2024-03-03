8d8e3e1b6b55a1e664946127192b6e8bafffb47d
Ilefkristou <159954471+Ilefkristou@users.noreply.github.com>
2024-03-03 12:16:08 +0100

    Update CalculatriceInterface.java

--- CalculatriceInterface.java
+++ CalculatriceInterface.java
@@ -1,8 +1,10 @@
 import java.rmi.*;
 
 public interface CalculatriceInterface extends Remote {
+    // Déclaration des méthodes que le client peut appeler
     int addition(int a, int b) throws RemoteException;
     int soustraction(int a, int b) throws RemoteException;
     int multiplication(int a, int b) throws RemoteException;
     int division(int a, int b) throws RemoteException;
 }
+
