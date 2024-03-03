5c897e9a5c0dc1bb8ad8a56877397dce21a2f33d
Ilefkristou <159954471+Ilefkristou@users.noreply.github.com>
2024-03-03 12:16:44 +0100

    Update CalculatriceClient.java

--- CalculatriceClient.java
+++ CalculatriceClient.java
@@ -3,19 +3,23 @@ import java.rmi.*;
 public class CalculatriceClient {
 
     public static void main(String args[]) throws Exception {
+        // Recherche de l'objet RMI sur le serveur
         CalculatriceInterface calculatrice = (CalculatriceInterface) Naming.lookup("rmi://localhost/CalculatriceServer");
 
         System.out.println("Client RMI démarré.");
 
         boolean continuer = true;
 
+        // Boucle pour permettre au client de faire plusieurs calculs
         while (continuer) {
+            // Demande à l'utilisateur de saisir l'opération et les opérandes
             int choix = Integer.parseInt(System.console().readLine("Entrez le code de l'opération (1: Addition, 2: Soustraction, 3: Multiplication, 4: Division): "));
             int a = Integer.parseInt(System.console().readLine("Entrez le premier nombre : "));
             int b = Integer.parseInt(System.console().readLine("Entrez le deuxième nombre : "));
 
             int resultat = 0;
 
+            // Appel de la méthode appropriée sur l'objet RMI en fonction du choix de l'utilisateur
             switch (choix) {
                 case 1:
                     resultat = calculatrice.addition(a, b);
@@ -34,8 +38,10 @@ public class CalculatriceClient {
                     break;
             }
 
+            // Affichage du résultat
             System.out.println("Résultat : " + resultat);
 
+            // Demande à l'utilisateur s'il veut continuer
             String continuerStr = System.console().readLine("Voulez-vous continuer ? (oui/non): ");
             continuer = continuerStr.equalsIgnoreCase("oui");
         }
